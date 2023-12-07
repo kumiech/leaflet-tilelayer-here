c0eaa91857faf05f63a074fd68afc1c84c36d1e9
Łukasz Lech <lukasz.lech@addsecure.com>
2023-12-07 18:27:26 +0100

    Add new options from HERE Raster Tile API

--- leaflet-tilelayer-here.js
+++ leaflet-tilelayer-here.js
@@ -1,77 +1,81 @@
 L.TileLayer.HERE = L.TileLayer.extend({
 	options: {
 		subdomains: '1234',
-		minZoom: 2,
-		maxZoom: 18,
 
-		// option style: String = 'explore.day'
-		style: 'explore.day',
-
-		// option resource: String = 'base'
-		resource: 'base',
-
-		// option format: String = 'png8'
-		// Image format to be used (`png8`, `png`, or `jpg`)
-		format: 'png8',
+		minZoom: 0,
+		maxZoom: 20,
 
 		// option apiKey: String = ''
 		// Required option. The `apiKey` provided as part of the HERE credentials
 		apiKey: '',
 
+		// option style: String = 'explore.day'
+		// Map style to be used ('explore.day', 'explore.night', 'explore.satellite.day', 'lite.day', 'lite.night', 'lite.satellite.day', 'logistics.day', 'satellite.day', 'topo.day')
+		style: 'explore.day',
+
+		// option resource: String = 'base'
+		// Map resource to be used ('background', 'base', 'blank', 'label')
+		resource: 'base',
+
+		// option projection: String = 'mc'
+		projection: 'mc',
+
+		// option format: String = 'png8'
+		// Image format to be used (`png8`, `png`, or `jpeg`)
+		format: 'png8',
+
+		// option tileResolution: Number = 256
+		// Image size to be used (256, 512)
+		tileResolution: 256,
+
+		// option ppi: Number = 100
+		// Image ppi be used (100, 400)
+		ppi: 100,
+
 		// option features: String = ''
 		// Map Features
 		features: null,
 
-		// option mapVersion: String = ''
-		// Map Version
-		mapVersion: null,
-
-		// option language: String = ''
+		// option language: String = 'en'
 		// Map language
 		language: null
 	},
-
-
-	initialize: function initialize(options) {
+	initialize: function (options) {
 		options = L.setOptions(this, options);
-
-		options.tileResolution = 256;
 
 		if (L.Browser.retina) {
 			options.tileResolution = 512;
+			options.ppi = 400;
 		}
 
-		var tileUrl = 'https://maps.hereapi.com/v3/{resource}/mc/{z}/{x}/{y}/{format}?apiKey={apiKey}&style={style}&size={tileResolution}';
+		var tileUrl = 'https://maps.hereapi.com/v3/{resource}/{projection}/{z}/{x}/{y}/{format}?apiKey={apiKey}&style={style}&size={tileResolution}';
 		var copyrightUrl = 'https://maps.hereapi.com/v3/copyright?apiKey={apiKey}';
 
 		if (options.features) {
 			tileUrl += '&features={features}'
 		}
 
-		if (options.mapVersion) {
-			tileUrl += '&mv={mapVersion}'
-		}
-
-		if (options.language) {
+		if (options.language && options.language !== 'en') {
 			tileUrl += '&lang={language}'
 		}
 
-		this._attributionUrl = L.Util.template(copyrightUrl, this.options);
+		if (options.ppi !== 100) {
+			tileUrl += '&ppi={ppi}';
+		}
 
+		this._attributionUrl = L.Util.template(copyrightUrl, this.options);
 		L.TileLayer.prototype.initialize.call(this, tileUrl, options);
 
 		this._attributionText = '';
 	},
-
-	onAdd: function onAdd(map) {
+	onAdd: function (map) {
 		L.TileLayer.prototype.onAdd.call(this, map);
 
 		if (!this._attributionBBoxes) {
 			this._fetchAttributionBBoxes();
 		}
 	},
-
-	onRemove: function onRemove(map) {
+	onRemove: function (map) {
 		L.TileLayer.prototype.onRemove.call(this, map);
 
 		if (this._map.attributionControl) {
@@ -80,19 +84,17 @@ L.TileLayer.HERE = L.TileLayer.extend({
 
 		this._map.off('moveend zoomend resetview', this._findCopyrightBBox, this);
 	},
-
-	_fetchAttributionBBoxes: function _onMapMove() {
+	_fetchAttributionBBoxes: function () {
 		var xmlhttp = new XMLHttpRequest();
 		xmlhttp.onreadystatechange = L.bind(function () {
-			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
 				this._parseAttributionBBoxes(JSON.parse(xmlhttp.responseText));
 			}
 		}, this);
-		xmlhttp.open("GET", this._attributionUrl, true);
+		xmlhttp.open('GET', this._attributionUrl, true);
 		xmlhttp.send();
 	},
-
-	_parseAttributionBBoxes: function _parseAttributionBBoxes(json) {
+	_parseAttributionBBoxes: function (json) {
 		if (!this._map) { return; }
 		var providers = json.copyrights.in;
 
@@ -108,28 +110,29 @@ L.TileLayer.HERE = L.TileLayer.extend({
 		this._map.on('moveend zoomend resetview', this._findCopyrightBBox, this);
 
 		this._attributionProviders = providers;
-
 		this._findCopyrightBBox();
 	},
+	_findCopyrightBBox: function () {
+		if (!this._map) {
+			return;
+		}
 
-	_findCopyrightBBox: function _findCopyrightBBox() {
-		if (!this._map) { return; }
 		var providers = this._attributionProviders;
 		var visibleProviders = [];
 		var zoom = this._map.getZoom();
 		var visibleBounds = this._map.getBounds();
 
-		for (var i = 0; i < providers.length; i++) {
-			if (providers[i].minLevel < zoom && providers[i].maxLevel > zoom)
-
+		for (let i = 0; i < providers.length; i++) {
+			if (providers[i].minLevel < zoom && providers[i].maxLevel > zoom) {
 				if (!providers[i].boundingBoxes) {
 					// No boxes = attribution always visible
 					visibleProviders.push(providers[i]);
 					break;
 				}
+			}
 
-			for (var j = 0; j < providers[i].boundingBoxes.length; j++) {
-				var box = providers[i].boundingBoxes[j];
+			for (let j = 0; j < providers[i].boundingBoxes.length; j++) {
+				const box = providers[i].boundingBoxes[j];
 				if (visibleBounds.overlaps(box)) {
 					visibleProviders.push(providers[i]);
 					break;
@@ -143,7 +146,7 @@ L.TileLayer.HERE = L.TileLayer.extend({
 			this._map.attributionControl.removeAttribution(this._attributionText);
 			this._map.attributionControl.addAttribution(this._attributionText = attributionText);
 		}
-	}
+	},
 });
 
 
